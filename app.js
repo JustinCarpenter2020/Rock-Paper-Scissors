151545e93e0384d7d0697686088b35354b66e4b3
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2020-12-09 14:10:56 -0700

    initilization

---
+++ app.js
@@ -0,0 +1,21 @@
+function play(){
+  
+}
+
+
+
+function playerChoice(){
+
+
+
+
+}
+
+
+
+const choices = ["Rock", "Paper", "Scissors"];
+function computer(){
+  choices [Math.floor(Math.random() * Math.floor(choices.length))]
+  console.log(choices)
+}
+
