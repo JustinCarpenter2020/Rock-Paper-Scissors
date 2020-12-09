0816fd28e640535ed8289485ef71da0908675107
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2020-12-09 14:58:42 -0700

    general js done

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
-function play(){
-  
-}
+let playerSelection = ""
 
-
-
-function playerChoice(){
-
-
-
-
+function player(play) {
+  if (play == 'rock') {
+    playerSelection = "Rock"
+  } else if (play == 'scissors') {
+    playerSelection = "Scissors"
+  } else if (play == 'paper') {
+    playerSelection = "Paper"
+  }
 }
 
 
 
 const choices = ["Rock", "Paper", "Scissors"];
-function computer(){
-  choices [Math.floor(Math.random() * Math.floor(choices.length))]
-  console.log(choices)
+
+function computer() {
+  choices[Math.floor(Math.random() * Math.floor(choices.length))]S
+}
+
+const computerSelection = computer();
+
+function play(playerSelection, computerSelection) {
+  if (playerSelection == 'Rock' && computerSelection === 'Scissors' || playerSelection == 'Paper' && computerSelection === 'Rock' || playerSelection == 'Scissors' && computerSelection === 'Paper') {
+    return "Victory!!";
+  } else if (playerSelection == 'Rock' && computerSelection === 'Paper' || playerSelection == 'Paper' && computerSelection === 'Scissors' || playerSelection == 'Scissors' && computerSelection === 'Rock') {
+    return "Defeat";
+  } else if (playerSelection == computerSelection) {
+    return "Draw";
+  }
+  console.log("Player chooses: " + playerSelection);
+  console.log("Computer chooses: " + computerSelection);
+  console.log(play())
 }
 
